1b95e5f97f636c990d2d514a348d3e2aa75c7c4c
Nils <Nils@i53337u-UB.fritz.box>
2015-10-19 15:34:44 +0200

    Hexapod.class created

---
+++ Stewart_Plattform_IPF/Hexapod.cpp
@@ -0,0 +1,17 @@
+/*
+ * Hexapod.cpp
+ *
+ *  Created on: 19.10.2015
+ *      Author: Nils
+ */
+#include "Hexapod.h"
+
+Hexapod::Hexapod() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Hexapod::~Hexapod() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Stewart_Plattform_IPF/Hexapod.h
@@ -0,0 +1,18 @@
+/*
+ * Hexapod.h
+ *
+ *  Created on: 19.10.2015
+ *      Author: Nils
+ */
+#ifndef HEXAPOD_H_
+#define HEXAPOD_H_
+
+#include "Arduino.h"
+
+class Hexapod {
+public:
+	Hexapod();
+	virtual ~Hexapod();
+};
+
+#endif /* HEXAPOD_H_ */
