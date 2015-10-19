4b37ae3d3fd16a5c596e3a39015a34677597da45
Nils <Nils@>
2015-10-19 18:10:45 +0200

    Beginn der Hexapodklasse

--- Stewart_Plattform_IPF/Hexapod.cpp
+++ Stewart_Plattform_IPF/Hexapod.cpp
@@ -6,12 +6,60 @@
  */
 #include "Hexapod.h"
 
-Hexapod::Hexapod() {
-	// TODO Auto-generated constructor stub
-
+Hexapod::Hexapod(float x_u, float y_u, float z_u, float x_o, float y_o,
+		float z_o, float LOA, float LUA, int W1, int W2, int W3, int W4, int W5,
+		int W6) {
+	for (int i = 0; i < 6; i++) {
+		//Vektor des unteren Befestigungspunkts
+		arme[i].AnkerUnten.x = x_u;
+		arme[i].AnkerUnten.y = y_u;
+		arme[i].AnkerUnten.z = z_u;
+		//Vektor des oberen Befestigungspunktes
+		arme[i].AnkerOben.x = x_o;
+		arme[i].AnkerOben.x = x_o;
+		arme[i].AnkerOben.x = x_o;
+		//Definieren der Laengen
+		arme[i].LaengeOberarm = LOA;
+		arme[i].LaengeUnterarm = LUA;
+		//Auswahl des korrekten Winkels
+		switch (i) {
+		case 0:
+			arme[i].WinkelAusrichtung = W1;
+			break;
+		case 1:
+			arme[i].WinkelAusrichtung = W2;
+			break;
+		case 2:
+			arme[i].WinkelAusrichtung = W3;
+			break;
+		case 3:
+			arme[i].WinkelAusrichtung = W4;
+			break;
+		case 4:
+			arme[i].WinkelAusrichtung = W5;
+			break;
+		case 5:
+			arme[i].WinkelAusrichtung = W6;
+			break;
+		}
+	}
 }
 
 Hexapod::~Hexapod() {
 	// TODO Auto-generated destructor stub
+}
+	/*
+	 * Methode berechnet den Vektor aus Rotationsmatrix u. Vektor des oberen Ankerpunktes.
+	 */
+Vector Hexapod::calcRotMatrix(Vector b, float yaw, float pitch, float roll) {
+	Vector c;
+	c.x = (cos(yaw)*cos(pitch)*b.x) +
+	c.y = (sin(yaw)*cos(pitch)*b.y) +
+	c.z = ((-sin(pitch))*b.z) +
+return c;
+}
+
+Hexapod::verfahren(float xx,float yy,float zz, float yawAngel, float pitchAngel, float rollAngel) {
+
 }
 
--- Stewart_Plattform_IPF/Hexapod.h
+++ Stewart_Plattform_IPF/Hexapod.h
@@ -7,12 +7,45 @@
 #ifndef HEXAPOD_H_
 #define HEXAPOD_H_
 
+#define DEG2RAD      (PI/180.0f)
+#define RAD2DEG      (180.0f/PI)
+
 #include "Arduino.h"
+#include "Vector.h"
+
 
 class Hexapod {
+
+
 public:
-	Hexapod();
+
+	Hexapod(float,float,float,float,float,float,float,float,int,int,int,int,int,int);
+
 	virtual ~Hexapod();
+
+	void verfahren(float, float, float, float, float,float);
+
+private:
+
+	/*
+	 * Dateityp Arm fuer die Spaetere Zusamenfassung geometrischer Merkmale.
+	 */
+	struct arm {
+		Vector AnkerUnten;
+		Vector AnkerOben;
+		float LaengeOberarm;
+		float LaengeUnterarm;
+		int WinkelAusrichtung;
+	};
+
+	arm arme[6];
+
+	// TODO Hier noch neue Array von Klasse Steller hinzufuegen
+
+	Vector calcRotMatrix(Vector, float, float, float);
+
+
+
 };
 
 #endif /* HEXAPOD_H_ */
