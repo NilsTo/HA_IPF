9b7f96e17c9aab208d3fd8981d4f53591481e634
RB <rbrueckner93@github.com>
2015-10-20 13:25:52 +0200

    value matrix

--- Stewart_Plattform_IPF/Stewart_Plattform_IPF.cpp
+++ Stewart_Plattform_IPF/Stewart_Plattform_IPF.cpp
@@ -1,15 +1,23 @@
 // Do not remove the include below
 #include "Stewart_Plattform_IPF.h"
+#include "Hexapod.h"
 
-
+// benannt nach Etikettierung
+float basex[6] = { -86.56, -47.3, 47.3, 86.56, 39.25, -39.25 }
+float basey[6] = { -4.66, -72.64, -72.64, -4.66, 77.29, 77.29 }
+float topx[6] = { -58.7, -51.7, 51.7, 58.7, 7, -7 }
+float topy[6] = { -25.8, -37.9, -37.9, -25.8, 57.735, 57.735 }
+int beta[6] = { 240, 60, 150, 330, 0, 180 }
 //The setup function is called once at startup of the sketch
-void setup()
-{
+
+Hexapod nils();
+
+void setup() {
 // Add your initialization code here
+
 }
 
 // The loop function is called in an endless loop
-void loop()
-{
+void loop() {
 //Add your repeated code here
 }
