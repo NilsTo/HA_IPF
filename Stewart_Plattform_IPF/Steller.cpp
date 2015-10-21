382d306c9b2e3dcf105e3f515888810c3a29393e
RB <rbrueckner93@github.com>
2015-10-21 17:25:22 +0200

    New version of steller

--- Stewart_Plattform_IPF/Steller.cpp
+++ Stewart_Plattform_IPF/Steller.cpp
@@ -6,11 +6,6 @@
  */
 
 #include "Steller.h"
-
-// by setting ALWAYS_ATTACHED true, one is not able to
-// manually manipulate the Servos during operation anymore
-#define ALWAYS_ATTACHED (false)  // Konzept nicht ganz durchdacht?!
-
 /**
  *  Erstellt neuen Steller, eine Subklasse von Servo.
  *  Es müssen mindestens Informationen über die PIN-Belegung und
@@ -21,49 +16,86 @@ Steller::Steller() {
 
 Steller::~Steller() {
 	// Detach Servo if not in use anymore.
-	if (ALWAYS_ATTACHED) {
+	if (this->attached())
 		this->detach();
-	}
 }
 /**
- * int flat, int upright sind die Grenzen, die im Projekt angefahren werden duerfen
+ * int flat, int upright sind die Grenzen,
+ * die im Projekt angefahren werden duerfen
  */
 void Steller::setAngles(int flat, int upright) {
 	this->_upright = upright;
 	this->_flat = flat;
+	this->_angleSet = true;
 }
 
 /**
- *  anzusteuernder PWM-Pin und
+ *  Setter f. anzusteuernder PWM-Pin und
  *  auszulesender analog-Pin
  */
 void Steller::setPins(int analogPin, int pwmPin) {
 	this->_analogPin = analogPin;
 	this->_pwmPin = pwmPin;
+	this->_pinSet = true;
 }
 
 /**
  *  Private Methode zum Ermitteln der Limits.
  *  Ein Aufrufen dürfte nur durch eine Initialisierungsmethode
  *  laufen, wenn es also nicht stoert o.ae.
+ *
+ *  Wird im Zusammenhang mit Auslesen der Potis notwendig.
  */
 void Steller::setAnalogLimits() {
+	// Poti-Wert bei waagerechter Position
 	this->stelle(0);
 	this->_minAn = analogRead(_analogPin);
-	// Serial.Print(_minAn);
+	// Poti-Wert bei senkrechter Position
 	this->stelle(90);
 	this->_maxAn = analogRead(_analogPin);
-	// Serial.Print(_maxAn);
 }
 
 /**
- *  schließt den Servo an.
+ *  Beginnt das Schreiben eines PWM-Signals auf Servo-PWM-Pin.
+ *  Wird vorher auf 90° gesetzt
  */
 void Steller::attach() {
-	this->Servo::attach(_pwmPin);
+	// Anschluss des Servos ist nur nach Definition der
+	// Steller-Eigenschaften (zwei Winkel, zwei Pins) zulässig
+	if (_angleSet && _pinSet) {
+
+		/*
+		 * optional: in senkrechte Position fahren
+		 * Delay wird zwischen den einzelnen Servos abgewartet.
+		 * wenn das Verhalten als gewünscht angesehen wird,
+		 * ist eine Organisation & ein gemeinsamer delay aller
+		 * Servos durch die übergeordnete Methode notwendig.
+		 *
+		 *
+		 * Es ist möglich, ein initiales Schreiben auf den Servo zu unterdrücken:
+		 *
+		 * Quelle: (http://stackoverflow.com/questions/26695670/attach-arduino-servo-without-moving)
+		 *
+		 * Thanks to djUniversal's answer, I got it working so that the servo doesn't move when attached.
+		 * In case anyone else is looking to do the same, all I did was go into ServoTimer2.cpp and comment
+		 * out the first 3 lines of the initISR method:
+		 *
+		 *static void initISR()
+		 *{
+		 *    for(uint8_t i=1; i <= NBR_CHANNELS; i++) {  // channels start from 1
+		 *        writeChan(i, DEFAULT_PULSE_WIDTH);  // store default values
+		 *    }
+		 *
+		 */
+
+		this->stelle(90);
+		delay(500);
+		this->Servo::attach(_pwmPin);
+
+	}
 }
 
-void Steller::detach(){
+void Steller::detach() {
 	this->Servo::detach();
 }
 
@@ -75,12 +107,7 @@ void Steller::stelle(int angle) {
 		int setpoint = map(angle, 0, 90, _flat, _upright);
 		// Ausgabe des Servosignals
 		this->Servo::write(setpoint);
-		//wurde entfernt, weil Fehler!
-		Serial.println(setpoint);
-		Serial.println("upright");
-		Serial.println(_upright);
-		Serial.println("flat");
-		Serial.println(_flat);
+		this->_lastAngle = angle;
 		//delay(200); // TODO Wie lang muss der Servo wirklich angesteuert werden?
 
 	} else {
@@ -93,7 +120,7 @@ void Steller::stelle(int angle) {
  *  Gibt den aufgrund der Potentiometer-Spannung ermittelten Winkel des Servohorns zurueck.
  *  Sollten die Grenzen noch nicht gesetzt sein, gibt sie 1023 zurueck.
  */
-int Steller::getAngle() {
+int Steller::getAnalogAngle() {
 	if (_minAn != 0 && _maxAn != 0) {
 		int angle = map(analogRead(_analogPin), _minAn, _maxAn, 0, 90);
 		return angle;
@@ -102,4 +129,13 @@ int Steller::getAngle() {
 				"Limits are not known yet. Steller::setAnalogLimits() may be a good idea.");
 		return 1023;
 	}
+}
+
+/**
+ * gibt zuletzt geschriebenen Wert (ohne Mapping) zurueck,
+ * der am Ende von ::stelle jeweils in das Objekt geschrieben wird.
+ */
+
+int Steller::getLastAngle() {
+	return _lastAngle;
 }
--- Stewart_Plattform_IPF/Steller.h
+++ Stewart_Plattform_IPF/Steller.h
@@ -11,19 +11,21 @@
 #include <Arduino.h>
 #include <Servo.h>
 
-class Steller : public Servo {
+class Steller: Servo {
 public:
 	Steller();
 	virtual ~Steller();
 	void stelle(int angle);
 	void attach();
 	void detach();
-	int getAngle();
+	int getAnalogAngle();
+	int getLastAngle();
 	void setAngles(int flat, int upright);
 	void setPins(int analogPin, int pwmPin);
 private:
-	int _upright, _flat, _analogPin, _pwmPin, _minAn, _maxAn;
+	int _upright, _flat, _analogPin, _pwmPin, _minAn, _maxAn, _lastAngle;
 	void setAnalogLimits();
+	boolean _pinSet, _angleSet;
 };
 
 #endif /* STELLER_H_ */
