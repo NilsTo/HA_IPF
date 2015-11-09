08f0509d3e1d6602c996dccfaf0ad4185eebd4ba
rbrueckner93 <mail@ruben-brueckner.de>
2015-11-09 21:15:03 +0100

    Update Steller.h
    kommentar zu öffentlicher vererbung,
    bezeichnung der Argumente von setAnalogLimits ergänzt.

--- Stewart_Plattform_IPF/Steller.h
+++ Stewart_Plattform_IPF/Steller.h
@@ -13,6 +13,10 @@
 
 #define speed 0.2727272727f   //Geschwindigkeit eines Servos in [Grad/ms]
 
+// oeffentliche Vererbung wurde notwendig, da sonst ein Servo::detach() nicht funktionierte bzw.
+// als "weiterleitende Klasse" nicht imt Stil (methode(){this->Mutterklasse::Methode();}) 
+// funktionierte
+
 class Steller: public Servo {
 public:
 	Steller();
@@ -25,7 +29,7 @@ public:
 	bool aktiv();
 	void setAngles(int flat, int upright);
 	void setPins(int analogPin, int pwmPin);
-	void setAnalogLimits(int,int);
+	void setAnalogLimits(int minAn, int maxAn);
 	int getAnalogPin();
 private:
 	int _upright, _flat, _analogPin, _pwmPin, _minAn, _maxAn, _lastAngle;
